0455ee2b60ac69d548e764ad346fe5620192df55
darren <devnull@localhost>
2007-08-03 10:52:26 -0400

    [svn] code cleanup

--- spectromicroscopy/KonsoleGui.py
+++ spectromicroscopy/KonsoleGui.py
@@ -12,7 +12,9 @@ from PyQt4 import QtCore, QtGui
 from Konsole import Ui_Kontrol
 
 class MyKon(Ui_Kontrol,QtGui.QMainWindow):
+
     """Establishes a custom Console for interacting with the Computer"""
+
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setupUi(self)
@@ -22,13 +24,14 @@ class MyKon(Ui_Kontrol,QtGui.QMainWindow):
                                self.macrosave)
         QtCore.QObject.connect(self.changer, QtCore.SIGNAL("clicked()"),
                                self.change)
+
     def macrosave(self):
         fd = QtGui.QFileDialog(self)
         self.filename = fd.getSaveFileName()
         s = codecs.open(self.filename,'w','utf-8')
         s.write(unicode(self.KonsoleEm.toPlainText()))
         s.close()
-            
+
     def change(self):
         try:
             fd = QtGui.QFileDialog(self)
@@ -36,7 +39,7 @@ class MyKon(Ui_Kontrol,QtGui.QMainWindow):
             os.chdir(self.path)
         except: 
             os.system("dir")
-    
+
     def konsole(self):
         self.Display.append(">>>"+self.KonsoleEm.toPlainText())
         Kommands=self.KonsoleEm.toPlainText().split(";")
@@ -45,10 +48,10 @@ class MyKon(Ui_Kontrol,QtGui.QMainWindow):
             doingit=pexpect.run(Kommand)
             self.Display.append(doingit)
 
+            
 if __name__ == "__main__":
     print __file__
     app = QtGui.QApplication(sys.argv)
     myapp = MyKon()
     myapp.show()
     sys.exit(app.exec_())
-        
