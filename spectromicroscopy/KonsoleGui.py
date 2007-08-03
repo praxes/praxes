9cb0ba346fc6c4d6a2868cccd426a08a04131ed5
darren <devnull@localhost>
2007-08-03 10:51:23 -0400

    [svn] code cleanup in KonsoleGui.py

--- spectromicroscopy/KonsoleGui.py
+++ spectromicroscopy/KonsoleGui.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 ################################################################################
-import sys, os, codecs
-from os.path import isfile
-OS="linux"
-if sys.platform=="win32":
-    OS="Windows"
-    import subprocess as sp
-else:
-    from pexpect import run
 
-    
+import codecs
+import pexpect
+import os
+import sys
+
 
 #GUI
 from PyQt4 import QtCore, QtGui
@@ -44,33 +40,10 @@ class MyKon(Ui_Kontrol,QtGui.QMainWindow):
     def konsole(self):
         self.Display.append(">>>"+self.KonsoleEm.toPlainText())
         Kommands=self.KonsoleEm.toPlainText().split(";")
-        if OS=="windows":#long way around pexpect not working with Windows
-            for i in range(len(Kommands)):
-                cmd="%s"%Kommands[i]
-                try:
-                    p=sp.Popen(cmd,stdout=sp.PIPE,stderr=sp.PIPE)
-                    result=p.communicate()[0]
-                    self.Display.append(result)
-                except:
-                    oldpath=self.path
-                    newpath=os.path.join(os.path.expanduser("~"),
-                        "My Documents/labwork/testing/src")
-                    self.path=os.path.join(self.path,newpath)
-                    os.chdir(self.path)
-                    program="python SubKonsole.py"
-                    input=cmd+","+oldpath
-                    p=sp.Popen(program, stdin=sp.PIPE, stdout=sp.PIPE,
-                               stderr=sp.PIPE)
-                    (result,error)=p.communicate(input)
-                    self.Display.append(result)
-                    self.Display.append(error)
-                    os.chdir(oldpath)
-                    
-        else:
-            for i in range(len(Kommands)):
-                Kommand="%s"%Kommands[i]
-                doingit=run(Kommand)
-                self.Display.append(doingit)
+        for i in range(len(Kommands)):
+            Kommand="%s"%Kommands[i]
+            doingit=pexpect.run(Kommand)
+            self.Display.append(doingit)
 
 if __name__ == "__main__":
     print __file__
