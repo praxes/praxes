62117deb6eb8c5bb05cf5c1f9fe7f2a9d13f78a9
darren <devnull@localhost>
2007-08-07 13:30:27 -0400

    [svn] first pass over specrunner

--- spectromicroscopy/smpcore/specrunner.py
+++ spectromicroscopy/smpcore/specrunner.py
@@ -43,9 +43,14 @@ specclient expects to work with nemonics, not full motor names
 
 class TestSpecMotor(SpecMotor.SpecMotorA):
     
-    __state_strings__ = ['NOTINITIALIZED', 'UNUSABLE', 'READY', 'MOVESTARTED', 'MOVING', 'ONLIMIT']
+    __state_strings__ = ['NOTINITIALIZED',
+                         'UNUSABLE',
+                         'READY',
+                         'MOVESTARTED',
+                         'MOVING',
+                         'ONLIMIT']
     
-    def __init__(self, specName = None, specVersion = None):
+    def __init__(self, specName=None, specVersion=None):
         SpecMotor.SpecMotorA.__init__(self, specName, specVersion)
         self.getPosition()
 
@@ -81,7 +86,6 @@ class TestSpecMotor(SpecMotor.SpecMotorA):
         return self.specName
 
 
-
 class XrfSpecVarA(SpecVariable.SpecVariableA):
     
 #    def __init__(self, var, host_port):
@@ -107,13 +111,6 @@ class XrfSpecVarA(SpecVariable.SpecVariableA):
     def getVarName(self):
         return self.channelName[4:len(self.channelName)]
 
-    def getValue(self):
-        """Return the watched variable current value."""
-        if self.connection is not None:
-            chan = self.connection.getChannel(self.channelName)
-
-            return chan.read()
-
 
 class XrfSpecVar(SpecVariable.SpecVariable):
 
@@ -122,17 +119,9 @@ class XrfSpecVar(SpecVariable.SpecVariable):
         if DEBUG:
             print '%s connected at %s'%(self.getVarName(),
                                                host_port)
-    
+
     def getVarName(self):
         return self.channelName[4:len(self.channelName)]
-
-    def getValue(self):
-        """Return the watched variable current value."""
-        chan = self.connection.getChannel(self.channelName)
-        
-        val = chan.read()
-        
-        return val
 
 
 class TestSpecCommand(SpecCommand.SpecCommandA):
@@ -143,25 +132,24 @@ class TestSpecCommand(SpecCommand.SpecCommandA):
     def replyArrived(self, reply):
         self.Reply=reply
         if (reply.error):
-            print "command %s received an error message: %s"%(self.command, reply.data)
+            print "command %s received an error message: %s"% \
+                (self.command, reply.data)
         else:
             print "Command %s received a reply: %s"%(self.command, reply.data)
     
     def connected(self):
        print "Command %s connected"% self.command
-       self.Reply=None 
+       self.Reply = None
     
     def disconnected(self):
         print "Command %s disconnected"% self.command
                 
     def statusChanged(self, ready):
-        state=["In progress","Complete","Unknown"]
+        state = ["In progress","Complete","Unknown"]
         print "Status is %s"%state[ready]
     
     def get_Reply(self):
         return self.Reply
-
-
 
 
 class Indexer(XrfSpecVarA):
@@ -177,15 +165,6 @@ class Indexer(XrfSpecVarA):
         pass
 
 
-    
-"""
-This section is what actualy interacts with the motorsand GUI
-"""
-
-
-
-
-
 class SpecRunner:
     """Connects to a Spec host server and manipulates its motors
     
@@ -193,58 +172,61 @@ class SpecRunner:
     
     """
     
-    def __init__(self,parent=None,Debug=0,spechost='',specport='',motor_names=[]):
+    def __init__(self, parent=None, Debug=0, spechost='', specport='',
+                 motor_names=[]):
         """input a debug roll and parrent if needed by default all are 0 or None
         Debug--set to 1 it deactivates spec commands
-        roll --set to 1 it auto starts spec -s on roll.chess.cornell.edu and connects,
+        roll --set to 1 it auto starts spec -s on roll.chess.cornell.edu and 
+                    connects,
              --set to 2 it wont autostart but will autoconnect.
          
         """
         DEBUG=Debug
         self._param_names = ()
         self._spec = None
-        self._specPort=specport
-        self._specHost=spechost
-        self._var_strings=[]
-        self._var=[]
-        self._cmd_string=''
-        self._motors={}
-        self._exc=''
-        self._parameters={}
-        self._type_dict={}
-        self._param_names=['position','offset','sign','low_limit','high_limit']
-        self._motor=None
+        self._specPort = specport
+        self._specHost = spechost
+        self._var_strings = []
+        self._var = []
+        self._cmd_string = ''
+        self._motors = {}
+        self._exc = ''
+        self._parameters = {}
+        self._type_dict = {}
+        self._param_names = ['position',
+                             'offset',
+                             'sign',
+                             'low_limit',
+                             'high_limit']
+        self._motor = None
         if self._specHost and self._specPort:
             self.serverconnect()
-    
-        
-    
-    def set_spec_host(self,spechost):
-        self._specHost=spechost
+
+    def set_spec_host(self, spechost):
+        self._specHost = spechost
     
     def get_spec_host(self):
         return self._specHost
     
     def set_spec_port(self,port):
-        self._specPort=port
+        self._specPort = port
     
     def get_spec_port(self):
         return self._specPort
     
     def serverconnect(self):
-        print "Connecting..."
         try:
             self._spec = Spec.Spec(self._specHost + ":" + self._specPort, 500)
-            self._exc=SpecCommand.SpecCommandA('',
-                                            self._specHost+":"+self._specPort)
-            print "Connected!"
+            self._exc = SpecCommand.SpecCommandA('',
+                                self._specHost+":"+self._specPort)
+            
             try:
-                self._index=Indexer("NPTS",self._specHost+":"+self._specPort)
-                self._last_index=0
-                self._type_dict["NPTS"]="Async"
-                self._S=XrfSpecVar("S", self._specHost+":"+self._specPort)
-                self._Detector=XrfSpecVar("MCA_NAME",
-                                       self._specHost+":"+self._specPort)
+                self._index = Indexer("NPTS",self._specHost+":"+self._specPort)
+                self._last_index = 0
+                self._type_dict["NPTS"] = "Async"
+                self._S = XrfSpecVar("S", self._specHost+":"+self._specPort)
+                self._Detector = XrfSpecVar("MCA_NAME",
+                                            self._specHost+":"+self._specPort)
                 self.mca_data = XrfSpecVar("MCA_DATA", 
                                            self._specHost+":"+self._specPort,
                                            500)
@@ -253,49 +235,51 @@ class SpecRunner:
         except:
             print "Failed to Connect"
             return False
-    
-    
-    def exc(self,command_string):
+
+    def exc(self, command_string):
         if self._exc:
             self._exc.executeCommand(command_string)
-    
-    
-    def readmotors(self,names=[],type="Test"):
+
+    def readmotors(self, names=[], type="Test"):
         if names:
-            motornames=names
+            motornames = names
         else:
-            motornames=self._spec.getMotorsMne()
+            motornames = self._spec.getMotorsMne()
         for name in motornames:
-            if type=="Test":
+            if type == "Test":
                 self._motors[name] = TestSpecMotor(name,
-                                                self._specHost + ":" + self._specPort)
-            elif type=="Sync":
+                                                   self._specHost + ":" + \
+                                                   self._specPort)
+            elif type == "Sync":
                 self._motors[name] = SpecMotor.SpecMotor(name,
-                                                self._specHost + ":" + self._specPort,500)
-            elif type=="Async":
+                                                         self._specHost + \
+                                                         ":" + self._specPort,
+                                                         500)
+            elif type == "Async":
                 self._motors[name] = SpecMotor.SpecMotorA(name,
-                                                self._specHost + ":" + self._specPort)
-        self._type_dict[name]=type
+                                                          self._specHost + \
+                                                          ":" + self._specPort)
+        self._type_dict[name] = type
         
     def readParam(self,motor):
-        motor=self._motors[motor]
-        value=[]
+        motor = self._motors[motor]
+        value = []
         for param in self._param_names:
             try: 
                 value.append(motor.getParameter(param))
             except:
                 value.append("unable to get value")
-            self._parameters[motor]=value
+            self._parameters[motor] = value
         return self._parameters[motor]
     
     def get_motor_names(self): 
         return self._motors.keys()
     
-    def set_motor(self,motor_name):
+    def set_motor(self, motor_name):
         if motor_name in self._motors:
-            self._motor=self._motors[motor_name]
+            self._motor = self._motors[motor_name]
     
-    def get_motor(self,motor_name):
+    def get_motor(self, motor_name):
         if motor_name in self._motors:
             return self._motors[motor_name]
 
@@ -305,55 +289,61 @@ class SpecRunner:
         else:
             return None
     
-    def get_motor_limits(self,motor_name):
+    def get_motor_limits(self, motor_name):
         return self._motors[motor_name].getLimits()
     
     def get_motor_position(self, motor_name):
         return self._motors[motor_name].getPosition()
     
-    def status(self,motor_name):
+    def status(self, motor_name):
         return self._motors[motor_name].status()
     
     def get_params_names(self):
         return self._param_names
     
     def get_params_values(self,motor):
-        motor=self._motors[motor]
+        motor = self._motors[motor]
         return self._parameters[motor]
     
-    def set_var(self,var,type="Test"):
-        if type=="Test":
+    def set_var(self,var,type = "Test"):
+        if type == "Test":
             self._var.append(XrfSpecVarA(var, 
-                    self._specHost+":"+self._specPort))
-        elif type=="Sync":
+                                         self._specHost+":"+self._specPort))
+        elif type == "Sync":
             self._var.append(XrfSpecVar(var, 
-                    self._specHost+":"+self._specPort,500))
-        elif type=="Async":
+                                        self._specHost+":"+self._specPort,500))
+        elif type == "Async":
             self._var.append(XrfSpecVarA(var, 
-                    self._specHost+":"+self._specPort))
-        elif type=="Index":
+                                         self._specHost+":"+self._specPort))
+        elif type == "Index":
             self._var.append(Indexer(var, self._specHost+":"+self._specPort))
         self._var_strings.append(var)
-        self._type_dict[var]=type
+        self._type_dict[var] = type
     
     def get_var(self):
         return self._var_strings
     
     def reset_var(self):
-        self._var=[]
-        self._var_strings=[]
+        self._var = []
+        self._var_strings = []
         print "Select a New Variable"
     
-    def set_cmd(self,cmd,type="Test"):
-        self._cmd_string=cmd
+    def set_cmd(self, cmd, type="Test"):
+        self._cmd_string = cmd
         self._cmd_list = [str(i) for i in self._cmd_string.split(' ')]
-        if type=="Test":
-            self._cmd=TestSpecCommand(self._cmd_list[0], self._specHost+":"+self._specPort)
-        if type=="Sync":
-            self._cmd=SpecCommand.SpecCommand(self._cmd_list[0], self._specHost+":"+self._specPort,500)
-        if type=="Async":
-            self._cmd=SpecCommand.SpecCommandA(self._cmd_list[0], self._specHost+":"+self._specPort)
-        self._type_dict[cmd]=type
+        if type == "Test":
+            self._cmd = TestSpecCommand(self._cmd_list[0],
+                                        self._specHost+":"+self._specPort)
+        if type == "Sync":
+            self._cmd = SpecCommand.SpecCommand(self._cmd_list[0],
+                                                self._specHost+":"+\
+                                                    self._specPort,
+                                                500)
+        if type == "Async":
+            self._cmd = SpecCommand.SpecCommandA(self._cmd_list[0],
+                                                 self._specHost+":"+\
+                                                    self._specPort)
+        self._type_dict[cmd] = type
     
     def get_cmd(self):
         return self._cmd_string
@@ -367,47 +357,45 @@ class SpecRunner:
     def run_cmd(self):
 #        print 'running command: %s'% ' '.join(self._cmd_list)
         self._cmd(*self._cmd_list[1:])
-        self._last_index=-1
+        self._last_index = -1
     
     def update(self):
         SpecEventsDispatcher.dispatch()
     
     def get_values(self):
-        values=[]
+        values = []
         prev = self._last_index
         curr = self._index.getValue()
         if curr != prev:
             if curr > prev+1:
                 print "missed point %s v %s"%(prev,curr)
-                self._last_index=curr
+                self._last_index = curr
                 return ([''],curr,'')
             else:
-                if self._Detector.getValue()=="vortex":
-                    a=self.compensate()
+                if self._Detector.getValue() == "vortex":
+                    a = self.compensate()
                     print 'dead time correction: %.3f'% a
                 else:
-                    a=1.0
+                    a = 1.0
 #                time.sleep(TIMEOUT)
                 counts = self.mca_data.getValue()
                 values.append(a*counts)
-                self._last_index=curr
+                self._last_index = curr
 #                    print "*****************Got Point***************"
-                if 1<=a:
-                    return (values,curr,True)
+                if 1 <= a:
+                    return (values, curr, True)
                 else:
-                    return (values,curr,False)
+                    return (values, curr, False)
         else:
-            return ([''],curr,'')
+            return ([''], curr, '')
 
     def compensate(self):
         S = self._S.getValue()
-        icr=float(S["5"])
-        ocr=float(S["7"])
-        real=float(S["8"])
-        live=float(S["9"])
+        icr = float(S["5"])
+        ocr = float(S["7"])
+        real = float(S["8"])
+        live = float(S["9"])
         return icr/ocr*real/live
-    
-
 
     def EmergencyStop(self):
         if self.get_cmd():
@@ -415,8 +403,6 @@ class SpecRunner:
             self.set_cmd('')
             print "\n %%%%%%%%%%%%ALL STOP%%%%%%%%%%%%"
 
-    
 
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     print "dont start as main"
