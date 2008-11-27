b50fd6e32d22ca47b2cde22907e6dd0d7c1ed86a
Darren Dale <darren@waterhouse>
2008-11-27 10:32:20 -0500

    tag 2008-11-27 (version 0.7) release

--- xpaxs/__init__.py
+++ xpaxs/__init__.py
@@ -78,7 +78,7 @@ from xpaxs import config
 # Normal code begins
 #---------------------------------------------------------------------------
 
-__version__ = '0.6(bzr)'
+__version__ = '0.7'
 
 # One of DEBUG, INFO, WARNING, ERROR, CRITICAL:
 logLevel = DEBUG
