48adbb0fafcde2aed846b35c2322d473cb826021
Darren Dale <darren.dale@cornell.edu>
2008-12-08 18:17:39 -0500

    revisiting the nexus interface

--- xpaxs/io/nexus/file.py
+++ xpaxs/io/nexus/file.py
@@ -26,6 +26,7 @@ from h5py import Dataset, File
 
 from .dataset import NXdataset
 from .registry import registry
+from .group import NXgroup
 
 #---------------------------------------------------------------------------
 # Normal code begins
@@ -33,24 +34,59 @@ from .registry import registry
 
 
 def getLocalTime():
+    """return a string representation of the local time"""
     res = list(time.localtime())[:6]
     g = time.gmtime()
     res.append(l[3]-g[3])
     return '%d-%02d-%02dT%02d:%02d:%02d%+02d:00'%tuple(res)
 
 
-class NXfile(File):
+class DummyLock(object):
+    def __enter__(self):
+        return 0
 
-    """
-    """
+    def __exit__(self, a, b, c):
+        return 0
+
+    def acquire(self, blocking=1):
+        return 1
+
+    def release(self):
+        return 0
+
+
+class NXfile(NXGroup):
+
+    def __init__(self, name, mode, lock=None):
+        """
+        Create a new file object.
+
+        Valid modes (like Python's file() modes) are:
+        - r   Readonly, file must exist
+        - r+  Read/write, file must exist
+        - w   Create file, truncate if exists
+        - w-  Create file, fail if exists
+        - a   Read/write if exists, create otherwise (default)
+
+        lock is a recursive thread lock conformant to python's context manager
+        """
+        if lock is None:
+            self._lock = DummyLock()
+        else:
+            assert hasattr(lock, __enter__)
+            assert hasattr(lock, __exit__)
+            self._lock = lock
+
+        self._h5 = h5py.File(name, mode)
+        super(NXfile, self).__init__(self, '/')
 
     def __getitem__(self, name):
         with self._lock:
             # a little hackish, for now:
-            item = super(NXfile, self).__getitem__(name)
+            item = super(File, self).__getitem__(name)
             if isinstance(item, Dataset):
                 nxclass = NXdataset
             else:
                 nxclass = registry[item.attrs['NX_class']]
-            del item
-            return nxclass(self, name)
+
+            return nxclass(self, item)
--- xpaxs/io/nexus/group.py
+++ xpaxs/io/nexus/group.py
@@ -15,7 +15,7 @@ from __future__ import absolute_import, with_statement
 # Extlib imports
 #---------------------------------------------------------------------------
 
-from h5py import Dataset, Group
+import h5py
 
 #---------------------------------------------------------------------------
 # xpaxs imports
@@ -29,18 +29,42 @@ from .registry import registry
 #---------------------------------------------------------------------------
 
 
-class NXgroup(Group):
+class NXgroup(object):
 
     """
     """
+
+    def __init__(self, parent_object, name, data=None):
+        """
+        If create is None, return an existing group or raise an error
+
+        If create is not None, it must be a python dictionary. hdf5 attributes
+        can be identified by::
+
+            data={'attrs': {'foo':1, 'bar':2}}
+
+        """
+        with parent_object._lock:
+            if name in self._h5:
+                self._h5 = h5py.Group(parent_object._h5, name, create=False)
+            else:
+                self._h5 = h5py.Group(parent_object._h5, name, create=True)
+
+            for attr, val in kwargs.pop(attrs, {}):
+                self.attrs[attr] = val
+
+            for key, val in data:
+                nxclassName, nxdata = val
+                if nxclassName == 'Dataset':
+                    NXdataset(self, key, nxdata)
+                else:
+                    nxclass = registry[]
 
     def __getitem__(self, name):
         with self._lock:
-            # a little hackish, for now:
-            item = super(NXgroup, self).__getitem__(name)
-            if isinstance(item, Dataset):
+            item = self._h5[name]
+            if isinstance(item, h5py.Dataset):
                 nxclass = NXdataset
             else:
                 nxclass = registry[item.attrs['NX_class']]
-            del item
-            return nxclass(self, name)
+            return nxclass(self, item)
