4b893b8cebe29545e02efd87abe7dbb1e3037ca0
Darren Dale <darren@waterhouse>
2008-12-10 21:49:40 -0500

    fixed import bug

--- xpaxs/io/nexus/__init__.py
+++ xpaxs/io/nexus/__init__.py
@@ -8,10 +8,10 @@ from .characterization import NXcharacterization
 from .component import (NXaperture, NXattenuator, NXbeam_stop, NXbending_magnet,
                         NXcollimator, NXcrystal, NXdetector, NXdisk_chopper,
                         NXfermi_chopper, NXfilter, NXflipper, NXguide,
-                        NXinsertion_device, NXmirror, NXmoderator, NXmonitor,
+                        NXinsertion_device, NXmirror, NXmoderator,
                         NXmonochromator, NXpolarizer, NXpositioner, NXsource,
                         NXvelocity_selector)
-from .data import NXdata, NXevent_data
+from .data import NXdata, NXevent_data, NXmonitor
 from .dataset import NXdataset
 from .entry import NXentry
 from .environment import NXenvironment
