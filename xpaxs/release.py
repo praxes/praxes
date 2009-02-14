39f42e1d59eede71af9605a200567c1949ce3a3c
Darren Dale <dsdale24@gmail.com>
2009-02-14 14:19:39 -0500

    tag 0.8a4, bump trunk to 0.8a5

--- xpaxs/release.py
+++ xpaxs/release.py
@@ -19,7 +19,7 @@ name = 'xpaxs'
 # bdist_deb does not accept underscores (a Debian convention).
 
 development = False    # change this to False to do a release
-version_base = '0.8a4'
+version_base = '0.8a5'
 branch = 'xpaxs'
 revision = '550'
 
