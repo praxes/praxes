2ce502edb6be05c9544265e6e33bda2d7aae2bc2
Darren Dale <dsdale24@gmail.com>
2010-09-10 16:27:33 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from distutils.core import setup
+
+packages = []
+for dirpath, dirnames, filenames in os.walk('praxis'):
+    if '__init__.py' in filenames:
+        packages.append('.'.join(dirpath.split(os.sep)))
+    else:
+        del(dirnames[:])        
+
+
+setup(
+    author = 'Darren Dale',
+    author_email = 'darren.dale@cornell.edu',
+    description = 'Praxis framework for scientific analysis',
+    name = 'praxis',
+    packages = packages,
+)
