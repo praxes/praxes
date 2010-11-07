c2768a9838ef6c3adcace74d508d9053eccd0365
Darren Dale <dsdale24@gmail.com>
2010-11-07 10:03:34 -0500

    added setup.py test command
    praxes test suite will be based on unittest2

--- setup.py
+++ setup.py
@@ -1,5 +1,34 @@
 import os
 from distutils.core import setup
+from distutils.cmd import Command
+
+
+
+class test(Command):
+
+    """ Run the test suite. Requires unittest2 """
+
+    description = "Run the test suite"
+
+    user_options = [('verbosity', 'v', 'set test report verbosity')]
+
+    def initialize_options(self):
+        self.verbosity = 0
+
+    def finalize_options(self):
+        try:
+            self.verbosity = int(self.verbosity)
+        except ValueError:
+            raise ValueError('Verbosity must be an integer.')
+
+    def run(self):
+        try:
+            import unittest2
+        except ImportError:
+            raise ImportError('unittest2 is required to run the test suite')
+
+        suite = unittest2.TestLoader().discover('praxes')
+        unittest2.TextTestRunner(verbosity=self.verbosity+1).run(suite)
 
 
 packages = []
@@ -13,6 +42,7 @@ for dirpath, dirnames, filenames in os.walk('praxes'):
 setup(
     author = 'Darren Dale',
     author_email = 'darren.dale@cornell.edu',
+    cmdclass = {'test': test},
     description = 'Praxes framework for scientific analysis',
     name = 'praxes',
     packages = packages,
