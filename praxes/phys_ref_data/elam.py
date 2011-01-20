2cc9dfb6d1a2a7b025160b4fb5aabad1ee7b1e3e
Darren Dale <dsdale24@gmail.com>
2011-01-20 17:14:59 -0500

    ORG: package cleanup

--- praxes/phys_ref_data/elam.py
+++ praxes/phys_ref_data/elam.py
@@ -18,9 +18,7 @@ import textwrap
 import numpy as np
 import quantities as pq
 
-from .interpolate import splint
-from . import base
-from .decorators import memoize
+from praxes.decorators import memoize
 
 
 class ElamSQLiteConnection(sqlite3.Connection):
@@ -437,6 +435,38 @@ class SplineInterpolable(object):
     def _log_independant_value(self):
         return self._get_data('log_energy')
 
+    def _splint(self, xa, ya, y2a, x):
+        '''spline interpolation'''
+        try:
+            len(x)
+        except TypeError:
+            x = np.array([x])
+
+        try:
+            klo, khi = np.array([
+                (np.flatnonzero(xa < i)[-1], np.flatnonzero(xa > i)[0])
+                for i in x
+                ]).transpose()
+        except IndexError:
+            raise ValueError(
+                'Input values must be between %s and %s'
+                % (np.exp(xa[0]), np.exp(xa[-1]))
+                )
+
+        h = xa[khi] - xa[klo]
+        if any(h <= 0):
+            raise ValueError, 'xa input must be strictly increasing'
+        a = (xa[khi] - x) / h
+        b = (x - xa[klo]) / h
+
+        res = (
+            a * ya[klo]
+            + b * ya[khi]
+            + ((a**3 - a) * y2a[klo] + (b**3 - b) * y2a[khi]) * (h**2) / 6
+            )
+        return res
+
+
     def __init__(self, element):
         self._element = element
 
@@ -446,7 +476,7 @@ class SplineInterpolable(object):
         log_ya = self._log_dependant_value
         log_yspline = self._log_dependant_value_spline
 
-        log_res = splint(log_xa, log_ya, log_yspline, log_energy)
+        log_res = self._splint(log_xa, log_ya, log_yspline, log_energy)
         return np.exp(log_res) * pq.cm**2 / pq.g
 
 
--- praxes/phys_ref_data/interpolate.py
+++ praxes/phys_ref_data/interpolate.py
@@ -5,37 +5,6 @@ import numpy as np
 from scipy import interpolate as _sp_interp
 
 
-def splint(xa, ya, y2a, x):
-    '''spline interpolation'''
-    try:
-        len(x)
-    except TypeError:
-        x = np.array([x])
-
-    try:
-        klo, khi = np.array([
-            (np.flatnonzero(xa < i)[-1], np.flatnonzero(xa > i)[0])
-            for i in x
-            ]).transpose()
-    except IndexError:
-        raise ValueError(
-            'Input values must be between %s and %s'
-            % (np.exp(xa[0]), np.exp(xa[-1]))
-            )
-
-    h = xa[khi] - xa[klo]
-    if any(h <= 0):
-        raise ValueError, 'xa input must be strictly increasing'
-    a = (xa[khi] - x) / h
-    b = (x - xa[klo]) / h
-
-    res = (
-        a * ya[klo]
-        + b * ya[khi]
-        + ((a**3 - a) * y2a[klo] + (b**3 - b) * y2a[khi]) * (h**2) / 6
-        )
-    return res
-
 def interpolate(x, ref_x, ref_y):
     try:
         shape = x.shape
