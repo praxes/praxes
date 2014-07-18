c42143d00908570ad1c42fb7c490ab8fad7a2675
Darren Dale <dsdale24@gmail.com>
2014-07-18 16:30:16 -0400

    adds conda recipe

---
+++ conda.recipe/build.sh
@@ -0,0 +1,4 @@
+$PYTHON praxes/physref/elam/create_db praxes/physref/elam/elam.dat praxes/physref/elam/elam.db
+$PYTHON praxes/physref/waasmaier/create_db praxes/physref/waasmaier/waasmaier_kirfel.dat praxes/physref/waasmaier/waasmaier_kirfel.db
+
+$PYTHON setup.py install
