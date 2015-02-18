d71cc10772546d5f1cf0b686a402b2f9f0b20f3d
Darren Dale <dsdale24@gmail.com>
2015-02-18 11:42:39 -0500

    conda recipe cleanup

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,10 +1,4 @@
-REM %PYTHON% praxes/physref/elam/create_db praxes/physref/elam/elam.dat praxes/physref/elam/elam.db
-REM %PYTHON% praxes/physref/waasmaier/create_db praxes/physref/waasmaier/waasmaier_kirfel.dat praxes/physref/waasmaier/waasmaier_kirfel.db
-
 %PYTHON% setup.py build
 
 %PYTHON% setup.py install --old-and-unmanageable
 if errorlevel 1 exit 1
-
-REM copy scripts\* %SCRIPTS%\
-REM if errorlevel 1 exit 1
